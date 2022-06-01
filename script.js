9fcc5567a8963e11684e8320b5a422c261550f06
Bruno e Sousa Alencar <brunoeesousanoleto2017@gmail.com>
2022-06-01 17:02:30 -0300

    terceira parte do projeto terminada

---
+++ script.js
@@ -0,0 +1,27 @@
+const init = () => {
+    const inputEmail = document.getElementById('email')  
+    const inputPassword = document.getElementById('senha')
+    const submitButton = document.getElementById('login')
+
+    if(submitButton){
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            fetch('https://reqres.in/api/login', {
+                method: 'POST',
+                headers: {
+                    'ContentType': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value,
+                })
+            }).then((Response) =>{
+                return Response.json();
+            }).then((data) => {
+                    console.log(data)
+                })
+        })
+    }
+}
+window.onload = init;
